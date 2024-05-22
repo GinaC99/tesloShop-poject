e8cbf302423959432a82d96af8af7541df537743
Gina Cortes <gipac2@hotmail.com>
2024-05-22 07:41:42 -0500

    Finalizacion relaciones bd SQL

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./product-image.entity";
 
 
 @Entity()
@@ -51,6 +52,13 @@ export class Product {
         default: []
     })
     tags:string[];
+
+    @OneToMany(
+        () => ProductImage,
+        (productImage)=> productImage.product,
+        {cascade:true}
+    )
+    image?:ProductImage
 
     @BeforeInsert()
     checkSlugInsert(){
